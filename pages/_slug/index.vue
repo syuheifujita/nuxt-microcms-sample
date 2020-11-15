a499d3187d51eb73ad8e47168887d88dd5399dcb
Syuhei Fujita <syuheifujita0117@gmail.com>
2020-11-16 00:58:15 +0900

    fix apiKey

---
+++ pages/_slug/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+    <div class="post" v-html="body"></div>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  async asyncData({ params }) {
+    const { data } = await axios.get(
+      `https://nuxt-sample.microcms.io/api/v1/news/${params.slug}`,
+      {
+        headers: { 'X-API-KEY': 'ef7399c4-fd48-46d2-92d9-634226fbd754' }
+      }
+    )
+    return data
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  async asyncData({$config}) {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://nuxt-sample.microcms.io/api/v1/news',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-API-KEY': $config.apiKey }
+      }
+    )
+    return data
+  }
+}
+</script>
